558f869ec7f545c55d253fb9f6d6097ffd17ae2e
Los-Kaleros <valekkristian2@gmail.com>
2022-03-26 20:52:22 +0100

    Pridal som postavicku + pridana fyzika hry s ovladanim

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import arcade
 
 
-
 #Rozmery + Nazov okna
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 900
 SCREEN_TITLE = "Lidl Mario"
 
+CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
+
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
 
     
 class MainHra(arcade.Window):
@@ -19,12 +23,20 @@ class MainHra(arcade.Window):
         self.player_sprite = None 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
+        self.physics_engine = None
+
     
     def setup(self):
         self.scene = arcade.Scene()
         
         self.scene.add_sprite_list("Player")
         self.scene.add_sprite_list("Walls", use_spatial_hash=True)
+
+        image_source = "./obrazky/Franta.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.scene.add_sprite("Player", self.player_sprite)
 
         for x in range(0, 1250, 64):
             wall = arcade.Sprite("./obrazky/zem.png", TILE_SCALING)
@@ -33,14 +45,43 @@ class MainHra(arcade.Window):
             self.scene.add_sprite("Walls", wall)
 
         coordinate_list = [[512, 96],[256, 96], [768, 96]]
+        
         for coordinate in coordinate_list:
             wall = arcade.Sprite("./obrazky/box1.png", TILE_SCALING)
             wall.position = coordinate
             self.scene.add_sprite("Walls", wall)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, gravity_constant = GRAVITY, walls = self.scene["Walls"])
+
         
     def on_draw(self):
         self.clear()
         self.scene.draw()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+
+        elif key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+
+    
     
     
     
