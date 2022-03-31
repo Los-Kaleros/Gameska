c5be589563d3264858c4e6ff5cad1da83e18ec52
Los-Kaleros <valekkristian2@gmail.com>
2022-03-31 19:36:58 +0200

    Pridane jednotlive obrazky z mapy (bez nich to nejde)

---
+++ tsx/box1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="box1" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Programovanie/Python/Private/Gameska/obrazky/box1.png" width="128" height="128"/>
+</tileset>
